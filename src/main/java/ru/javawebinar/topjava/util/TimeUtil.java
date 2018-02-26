330099f43244822c57e469e5cad0a5fb05d89268
Vitalie_Notebook <vitalie.jurjiu@gmail.com>
2018-02-26 10:40:21 -0500

    Prepare to HW0

--- src/main/java/ru/javawebinar/topjava/util/TimeUtil.java
+++ src/main/java/ru/javawebinar/topjava/util/TimeUtil.java
@@ -6,8 +6,10 @@ import java.time.LocalTime;
  * GKislin
  * 07.01.2015.
  */
-public class TimeUtil {
-    public static boolean isBetween(LocalTime lt, LocalTime startTime, LocalTime endTime) {
+public class TimeUtil
+{
+    public static boolean isBetween(LocalTime lt, LocalTime startTime, LocalTime endTime)
+    {
         return lt.compareTo(startTime) >= 0 && lt.compareTo(endTime) <= 0;
     }
 }
